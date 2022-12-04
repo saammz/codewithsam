b11ea71123ca6aa83eadac9f46af57ebfc12285e
ghosty47 <ghostcybertron@gmail.com>
2022-12-04 02:59:39 +0100

    updated work data collection

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -7,7 +7,7 @@ export const Work = [
         description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
         tags:["react","gsap","styledComponents"],
         
-        demo:"https://agencysiteapp.herokuapp.com/",
+        demo:"https://logan-agency.netlify.app",
         github:"https://github.com/ghosty47/Agencysite"
     },
     {
@@ -16,68 +16,88 @@ export const Work = [
         description:"A Google like keeper app to document your works built on ReactJs and React Hooks.",
         tags:["react","React Hooks"],
         
-        demo:"https://google-keeper-app-clone.herokuapp.com",
+        demo:"https://keeperappz.netlify.app",
         github:"https://github.com/ghosty47/keeper-app"
         
     },
     {
         id:3,
+        name:"Video conferencing app",
+        description:"A Conferencing app built with React and Typescript on top of the agora framwork SDK library",
+        tags:["react","React Hooks", "typescript", "agora SDK Library"],
+        
+        demo:"https://cwsvideoapp.netlify.app/create",
+        github:"https://github.com/ghosty47/videoapp"
+        
+    },
+    {
+        id:4,
+        name:"Typing speed application",
+        description:"A Typing speed application were a user can test his typing speed accuracy and performance",
+        tags:["react","React Hooks", "typescript", "tailwind-css"],
+        
+        demo:"https://typingzs.netlify.app/",
+        github:"https://github.com/ghosty47/myType"
+        
+    },
+    {
+        id:5,
         name:"Millionaire Quiz",
         description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
         tags:["react","api","sass"],
         
-        demo:"https://quiz-billionaire.herokuapp.com",
+        demo:"https://wwtbab.netlify.app",
         github:"https://github.com/ghosty47/billionareapp"
     },
     {
-        id:4,
+        id:6,
         name:"Universal-Dashboard",
         description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
         tags:["react","CSS","react-router, Material-UI"],
         
-        demo:"https://universal-dashboard.herokuapp.com",
+        demo:"https://dashyboardz.netlify.app",
         github:"https://github.com/ghosty47/adminDashboard"
        
-    },{
-        id:5,
-        name:"Huddle Landing Page",
-        description:"A responsive Huddle landing page built with ReactJs and styled-components.",
-        tags:["react","styled-components","CSS"],
-        
-        demo:"https://huddle-a.herokuapp.com/",
-        github:"https://github.com/ghosty47/Styled-components"
-        
-    },
-    {
-        id:6,
-        name:"An mini e-commerce website",
-        description:"An e-commerce shopping web application built from scratch with React",
-        tags:["react","react-router","css"],
-        
-        demo:"https://frooto-online-shop.herokuapp.com/",
-        github:"https://github.com/GodfreySam/waaw-e-commerce"
-        
     },
     {
         id:7,
-        name:"A feedback app",
-        description:"A simple yet beautiful feedback application that takes in customer or individual feedbacks and stores the information on the server",
-        tags:["react","react-router","css","context-API"],
+        name:"A Github finder",
+        description:"A React app to search GitHub profiles and see profile details, user repositories, and a lot more",
+        tags:["react","react-router","Tailwind-css","Daisy-UI","Context-API"],
         
-        demo:"https://customer-feedback-app.netlify.app/",
-        github:"https://github.com/ghosty47/feedbackapp"
+        demo:"https://findgithubsz.netlify.app",
+        github:"https://github.com/ghosty47/github-finder"
         
     },
     {
         id:8,
-        name:"A Github finder(still in progress)",
-        description:"A React app to search GitHub profiles and see profile details, user repositories, and a lot more",
-        tags:["react","react-router","Tailwind-css","Daisy-UI","Context-API"],
+        name:"An e-commerce website (in-Production)",
+        description:"An e-commerce shopping web application built from scratch with React",
+        tags:["react","react-router","css", "redux","tailwind"],
         
-        demo:"https://giithub-finder.herokuapp.com/",
-        github:"https://github.com/ghosty47/github-finder"
+        demo:"https://www.shopturbocart.com/",
+        github:"private"
         
     },
-    
+    {
+        id:9,
+        name:"A feedback app",
+        description:"A simple yet beautiful feedback application that takes in customer or individual feedbacks and stores the information on the server",
+        tags:["react","react-router","css","context-API"],
+        
+        demo:"https://feedbackappz.netlify.app",
+        github:"https://github.com/ghosty47/feedbackapp"
+        
+    },
+    {
+        id:10,
+        name:"Huddle Landing Page",
+        description:"A responsive Huddle landing page built with ReactJs and styled-components.",
+        tags:["react","styled-components","CSS"],
+        
+        demo:"https://huddlestyledz.netlify.app/",
+        github:"https://github.com/ghosty47/Styled-components"
+        
+    },
   
 ] 
